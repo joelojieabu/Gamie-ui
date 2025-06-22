298cd4b8a9106d93e1f3d8c587e358106189e1dd
softolumese <olumese.ojieabu@softcode.ng>
2025-06-22 19:24:10 +0100

    chore: update base url

--- src/app/core/environments/environment.ts
+++ src/app/core/environments/environment.ts
@@ -1,3 +1,3 @@
-// export const environment = 'https://kiddieslearn-api-production.up.railway.app/';
-export const environment = 'http://localhost:3000/';
+export const environment = 'https://kiddieslearn-api-production.up.railway.app/';
+// export const environment = 'http://localhost:3000/';
 
